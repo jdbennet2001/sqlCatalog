981b05ba496f4dda96a3f31ab84a0357d896c27d
Jonathan Bennett <jdbennet@ca.ibm.com>
2021-09-16 16:42:03 -0400

    JEST configuration

---
+++ test/jest.spec.js
@@ -0,0 +1,9 @@
+describe('Jest test scaffolding, demo agent (demonstrate code coverage)', () => {
+
+    it('main', async () => {
+        const config = 'demo'
+        const result = 'demo'
+        expect(result).toBe(config);
+    });
+
+ });
