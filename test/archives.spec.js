a2feef368eafb14a52d93c367f03cbba6a7908a5
Jonathan Bennett <jdbennet@ca.ibm.com>
2021-09-17 17:41:36 -0400

    Add support for scaling cover images

--- test/archives.spec.js
+++ test/archives.spec.js
@@ -2,7 +2,8 @@ const path = require('path');
 const fs = require('fs')
 
 const {cover, pages} = require('../lib/zip');
-const {refresh, save} = require('../lib/dataStore')
+const {refresh, save, get} = require('../lib/dataStore')
+const {resize} = require('../lib/image')
 
 const archive = path.join(__dirname, './data/IM-007.cbz')
 
@@ -31,10 +32,22 @@ describe('Abstracted CBZ workflow routines', () => {
 
         // Save the cover
         await save( archive, contents.length, page)
-        
-        expect(result).toBeDefined();
-    // let buff = new Buffer(data, 'base64');
 
+        // Get it again
+        let rows = await get(archive);
+
+        const [row,] = rows;
+        // let binData = new Buffer(row.cover, 'base64')
+        // fs.writeFileSync( 'bar.jpg', binData)
+        
+        expect(rows).toHaveLength(1);
+    })
+
+    it( 'can resize a cover', async () => {
+        const page = await cover(archive);
+        const thumbnail = await resize(page)
+        // fs.writeFileSync( 'th.jpg', thumbnail);
+        expect(thumbnail).toBeDefined();
     })
 
  });
